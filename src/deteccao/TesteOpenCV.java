d4b49dd664591f54da84c99d51e2acf24bacc14b
Mateus Ruas <mateusrduarte@gmail.com>
2020-04-08 18:30:32 -0300

    Conveção de imagem para escala de cinza.

--- src/deteccao/TesteOpenCV.java
+++ src/deteccao/TesteOpenCV.java
@@ -2,6 +2,7 @@ package deteccao;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
 import static org.opencv.imgcodecs.Imgcodecs.imread;
@@ -16,5 +17,9 @@ public class TesteOpenCV {
         Mat imagemColorida = imread("src\\deteccao\\original.jpg", CV_LOAD_IMAGE_COLOR);
         Utilitarios ut = new Utilitarios();
         ut.mostraImagem(ut.convertMatToImage(imagemColorida));
+
+        Mat imagemCinza = new Mat();
+        Imgproc.cvtColor(imagemColorida, imagemCinza, Imgproc.COLOR_BGR2GRAY);
+        ut.mostraImagem(ut.convertMatToImage(imagemCinza));
     }
 }

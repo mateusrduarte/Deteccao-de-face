2d8ff0ddae61f4ebd402eba00982e0bb38f3ab2c
Mateus Ruas <mateusrduarte@gmail.com>
2020-04-08 18:17:19 -0300

    Exibição de imagem em tela.

--- src/deteccao/TesteOpenCV.java
+++ src/deteccao/TesteOpenCV.java
@@ -1,6 +1,10 @@
 package deteccao;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
 
 public class TesteOpenCV {
 
@@ -8,5 +12,9 @@ public class TesteOpenCV {
 
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println(Core.VERSION);
+
+        Mat imagemColorida = imread("src\\deteccao\\original.jpg", CV_LOAD_IMAGE_COLOR);
+        Utilitarios ut = new Utilitarios();
+        ut.mostraImagem(ut.convertMatToImage(imagemColorida));
     }
 }
